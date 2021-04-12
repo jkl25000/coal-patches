c75895d8cb8f89bc1027b88d9e3468d5ade92c6c
jkl25000 <jkl2505000@gmail.com>
2021-04-11 23:16:49 -0400

    pbo unpacked and uploaded to git

---
+++ config.cpp
@@ -0,0 +1,252 @@
+class CfgPatches
+{
+	class coal_patches
+	{
+		units[]={};
+		weapons[]=
+		{};
+		magazines[]=
+		{};
+		requiredVersion=1.0;
+		requiredAddons[]=
+		{};
+		author="tanaKa";
+		authorUrl="https://coalitiongroup.net";
+		version="1.0";
+		versionStr="1.0";
+		versionAr[]={1,0,0,0};
+		ammo[]=
+		{};
+	};
+};
+
+class CfgUnitInsignia {
+	class aim
+	{
+		displayName = "[COALITION] Andok 'AthresInMemoriam' Cotkar";
+		author = "Konstantin";
+		texture = "\coal_patches\patches\aim.paa";
+	};
+	class medic
+	{
+		displayName = "[CMF] Medic";
+		author = "jkl25";
+		texture = "\coal_patches\patches\medic.paa";
+	};
+	class COY
+	{
+		displayName = "[CMF] Company Leader";
+		author = "jkl25";
+		texture = "\coal_patches\patches\coy.paa";
+	};
+	class PL
+	{
+		displayName = "[CMF] Platoon Leader";
+		author = "jkl25";
+		texture = "\coal_patches\patches\pl.paa";
+	};
+	class PL1
+	{
+		displayName = "[CMF] 1st Platoon Leader";
+		author = "jkl25";
+		texture = "\coal_patches\patches\pl1.paa";
+	};
+	class PL2
+	{
+		displayName = "[CMF] 2nd Platoon Leader";
+		author = "jkl25";
+		texture = "\coal_patches\patches\pl2.paa";
+	};
+	class ASL
+	{
+		displayName = "[CMF] Alpha Squad Leader";
+		author = "jkl25";
+		texture = "\coal_patches\patches\asl.paa";
+	};
+	class BSL
+	{
+		displayName = "[CMF] Bravo Squad Leader";
+		author = "jkl25";
+		texture = "\coal_patches\patches\bsl.paa";
+	};
+	class CSL
+	{
+		displayName = "[CMF] Charlie Squad Leader";
+		author = "jkl25";
+		texture = "\coal_patches\patches\csl.paa";
+	};
+	class DSL
+	{
+		displayName = "[CMF] Delta Squad Leader";
+		author = "jkl25";
+		texture = "\coal_patches\patches\dsl.paa";
+	};
+	class A1
+	{
+		displayName = "[CMF] Alpha 1";
+		author = "jkl25";
+		texture = "\coal_patches\patches\a1.paa";
+	};
+	class A2
+	{
+		displayName = "[CMF] Alpha 2";
+		author = "jkl25";
+		texture = "\coal_patches\patches\a2.paa";
+	};
+	class B1
+	{
+		displayName = "[CMF] Bravo 1";
+		author = "jkl25";
+		texture = "\coal_patches\patches\b1.paa";
+	};
+	class B2
+	{
+		displayName = "[CMF] Bravo 2";
+		author = "jkl25";
+		texture = "\coal_patches\patches\b2.paa";
+	};
+	class C1
+	{
+		displayName = "[CMF] Charlie 1";
+		author = "jkl25";
+		texture = "\coal_patches\patches\c1.paa";
+	};
+	class C2
+	{
+		displayName = "[CMF] Charlie 2";
+		author = "jkl25";
+		texture = "\coal_patches\patches\c2.paa";
+	};
+	class D1
+	{
+		displayName = "[CMF] Delta 1";
+		author = "jkl25";
+		texture = "\coal_patches\patches\d1.paa";
+	};
+	class D2
+	{
+		displayName = "[CMF] Delta 2";
+		author = "jkl25";
+		texture = "\coal_patches\patches\d2.paa";
+	};
+	class ENG
+	{
+		displayName = "[CMF] Engineer";
+		author = "jkl25";
+		texture = "\coal_patches\patches\eng.paa";
+	};
+	class LOGI
+	{
+		displayName = "[CMF] Logi";
+		author = "jkl25";
+		texture = "\coal_patches\patches\logi.paa";
+	};
+	class MMG
+	{
+		displayName = "[CMF] MMG";
+		author = "jkl25";
+		texture = "\coal_patches\patches\mmg.paa";
+	};
+	class HMG
+	{
+		displayName = "[CMF] HMG";
+		author = "jkl25";
+		texture = "\coal_patches\patches\hmg.paa";
+	};
+	class MAT
+	{
+		displayName = "[CMF] MAT";
+		author = "jkl25";
+		texture = "\coal_patches\patches\mat.paa";
+	};
+	class HAT
+	{
+		displayName = "[CMF] HAT";
+		author = "jkl25";
+		texture = "\coal_patches\patches\hat.paa";
+	};
+	class MTR
+	{
+		displayName = "[CMF] Mortar";
+		author = "jkl25";
+		texture = "\coal_patches\patches\mtr.paa";
+	};
+	class ARTY
+	{
+		displayName = "[CMF] Artillery";
+		author = "jkl25";
+		texture = "\coal_patches\patches\arty.paa";
+	};
+	class RECON
+	{
+		displayName = "[CMF] Recon";
+		author = "jkl25";
+		texture = "\coal_patches\patches\recon.paa";
+	};
+	class IFV
+	{
+		displayName = "[CMF] IFV";
+		author = "jkl25";
+		texture = "\coal_patches\patches\ifv.paa";
+	};
+	class TNK
+	{
+		displayName = "[CMF] Tank";
+		author = "jkl25";
+		texture = "\coal_patches\patches\tnk.paa";
+	};
+	class TH
+	{
+		displayName = "[CMF] Transport Helicopter";
+		author = "jkl25";
+		texture = "\coal_patches\patches\TH.paa";
+	};
+	class AH
+	{
+		displayName = "[CMF] Attack Helicopter";
+		author = "jkl25";
+		texture = "\coal_patches\patches\ah.paa";
+	};
+	class CAS
+	{
+		displayName = "[CMF] Close Air Support";
+		author = "jkl25";
+		texture = "\coal_patches\patches\cas.paa";
+	};
+	class WHITE
+	{
+		displayName = "[CMF] White Team";
+		author = "jkl25";
+		texture = "\coal_patches\patches\white.paa";
+	};
+	class RED
+	{
+		displayName = "[CMF] Red Team";
+		author = "jkl25";
+		texture = "\coal_patches\patches\red.paa";
+	};
+	class GREEN
+	{
+		displayName = "[CMF] Green Team";
+		author = "jkl25";
+		texture = "\coal_patches\patches\green.paa";
+	};
+	class BLUE
+	{
+		displayName = "[CMF] Blue Team";
+		author = "jkl25";
+		texture = "\coal_patches\patches\blue.paa";
+	};
+	class YELLOW
+	{
+		displayName = "[CMF] Yellow Team";
+		author = "jkl25";
+		texture = "\coal_patches\patches\yellow.paa";
+	};
+	class BLANK
+	{
+		displayName = "[CMF] Blank Patch";
+		author = "jkl25";
+		texture = "\coal_patches\patches\blank.paa";
+	};
+};
